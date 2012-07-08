c55fbf04493aa37c46bf9de62e3294b3d2999942
Brett Ryan <brett.ryan@gmail.com>
2012-07-09 06:43:26 +1000

    Basics for job task which will work in confluence 4.x.

---
+++ src/main/java/com/drunkendev/confluence/plugins/attachments/PurgeOldAttachmentsJob.java
@@ -0,0 +1,80 @@
+/*
+ * PurgeOldAttachmentsJob.java    Jul 9 2012, 04:31
+ *
+ * Copyright 2010 John Sands (Australia) Ltd. All rights reserved.
+ * Use is subject to license terms. This is an unpublished work.
+ */
+
+package com.drunkendev.confluence.plugins.attachments;
+
+import com.atlassian.confluence.pages.Attachment;
+import com.atlassian.confluence.pages.AttachmentManager;
+import com.atlassian.quartz.jobs.AbstractJob;
+import java.util.Calendar;
+import java.util.Iterator;
+import java.util.List;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Purge old attachment versions.
+ *
+ * @author  Brett Ryan
+ */
+public class PurgeOldAttachmentsJob extends AbstractJob {
+
+    private static final Logger LOG =
+            LoggerFactory.getLogger(PurgeOldAttachmentsJob.class);
+    private AttachmentManager attachmentManager;
+
+    /**
+     * Creates a new {@code PurgeAttachmentsJob} instance.
+     */
+    public PurgeOldAttachmentsJob() {
+        System.out.println("Creating purge-old-attachment-job instance.");
+    }
+
+    @Override
+    public void doExecute(JobExecutionContext jec) throws JobExecutionException {
+        System.out.println("Purge old attachments started.");
+
+        // To delete old attachments 1 week ago.
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.DAY_OF_MONTH, 7);
+
+        if (attachmentManager == null) {
+            System.out.println("attachment manager is null, why?");
+            return;
+        }
+
+        // Assuming attm is an injected AttachmentManager object
+        Iterator<Attachment> i =
+                attachmentManager.getAttachmentDao().
+                findLatestVersionsIterator();
+        while (i.hasNext()) {
+            Attachment a = i.next();
+            if (a.getVersion() > 1) {
+                List<Attachment> prior = attachmentManager.
+                        getPreviousVersions(a);
+                for (Attachment p : prior) {
+                    if (c.after(p.getLastModificationDate())) {
+                        // Log removal
+                        System.out.println("Would remove attachment: "
+                                + p.getDisplayTitle() + " (" + p.getExportPath() + ")");
+                        //attachmentManager.removeAttachmentFromServer(p);
+                    }
+                }
+            }
+        }
+
+        System.out.println("Purge old attachments completed.");
+    }
+
+    public void setAttachmentManager(AttachmentManager attachmentManager) {
+        this.attachmentManager = attachmentManager;
+    }
+
+}
