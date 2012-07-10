7dc9fa9f92140b4c983cc3fe1a391ae6e3b7ef23
Brett Ryan <brett.ryan@johnsands.com.au>
2012-07-10 19:12:34 +1000

    First cut of implementing settings to action.

--- src/main/java/com/drunkendev/confluence/plugins/attachments/PurgeAttachmentsJob.java
+++ src/main/java/com/drunkendev/confluence/plugins/attachments/PurgeAttachmentsJob.java
@@ -9,6 +9,8 @@ package com.drunkendev.confluence.plugins.attachments;
 
 import com.atlassian.confluence.pages.Attachment;
 import com.atlassian.confluence.pages.AttachmentManager;
+import com.atlassian.confluence.spaces.Space;
+import com.atlassian.confluence.spaces.SpaceManager;
 import com.atlassian.quartz.jobs.AbstractJob;
 import java.util.Calendar;
 import java.util.Iterator;
@@ -29,12 +31,26 @@ public class PurgeAttachmentsJob extends AbstractJob {
     private static final Logger LOG =
             LoggerFactory.getLogger(PurgeAttachmentsJob.class);
     private AttachmentManager attachmentManager;
+    private SpaceManager spaceManager;
+    private PurgeAttachmentsSettingsService settingSvc;
 
     /**
      * Creates a new {@code PurgeAttachmentsJob} instance.
      */
     public PurgeAttachmentsJob() {
         System.out.println("Creating purge-old-attachment-job instance.");
+    }
+
+    public void setAttachmentManager(AttachmentManager attachmentManager) {
+        this.attachmentManager = attachmentManager;
+    }
+
+    public void setSpaceManager(SpaceManager spaceManager) {
+        this.spaceManager = spaceManager;
+    }
+
+    public void setPurgeAttachmentsSettingsService(PurgeAttachmentsSettingsService purgeAttachmentsSettingsService) {
+        this.settingSvc = purgeAttachmentsSettingsService;
     }
 
     @Override
@@ -51,13 +67,31 @@ public class PurgeAttachmentsJob extends AbstractJob {
             return;
         }
 
+        PurgeAttachmentSettings dflt = settingSvc.getSettings(null);
+
+        //TODO: Not sure if I can do this at the moment.
+//        List<Space> spaces = spaceManager.getAllSpaces();
+//        for (Space space : spaces) {
+//            PurgeAttachmentSettings sng = getSettings(space, dflt);
+//            processSpace(space, sng);
+//        }
+
+
+        //TODO: Need to work out processing by space instead of all at a time.
         // Assuming attm is an injected AttachmentManager object
         Iterator<Attachment> i =
                 attachmentManager.getAttachmentDao().
                 findLatestVersionsIterator();
         while (i.hasNext()) {
             Attachment a = i.next();
+            PurgeAttachmentSettings stng = getSettings(a, dflt);
+
+            if (stng == null) {
+                continue;
+            }
+
             if (a.getVersion() > 1) {
+                // Change this to a rules based mechanism instead.
                 List<Attachment> prior = attachmentManager.
                         getPreviousVersions(a);
                 for (Attachment p : prior) {
@@ -78,8 +112,34 @@ public class PurgeAttachmentsJob extends AbstractJob {
         System.out.println("Purge old attachments completed.");
     }
 
-    public void setAttachmentManager(AttachmentManager attachmentManager) {
-        this.attachmentManager = attachmentManager;
+    //TODO: Not sure if I can do this at the moment.
+    private void processSpace(Space space, PurgeAttachmentSettings sng) {
+        //attachmentManager.getAttachmentDao().
+        //spaceManager.get
+        //Iterator<Attachment> i =
+        //        attachmentManager.getAttachmentDao()
+        //        .
+        //        //findLatestVersionsIterator();
+    }
+
+    private PurgeAttachmentSettings getSettings(Attachment a, PurgeAttachmentSettings dflt) {
+        if (a == null) {
+            return null;
+        }
+        return getSettings(a.getSpace(), dflt);
+    }
+
+    private PurgeAttachmentSettings getSettings(Space space, PurgeAttachmentSettings dflt) {
+        if (space == null) {
+            return null;
+        }
+        PurgeAttachmentSettings sng = settingSvc.getSettings(space.getKey());
+        if (sng != null && sng.getMode() == PurgeAttachmentSettings.MODE_DISABLED) {
+            sng = null;
+        } else if (sng == null || sng.getMode() == PurgeAttachmentSettings.MODE_GLOBAL) {
+            sng = dflt;
+        }
+        return sng;
     }
 
 }
